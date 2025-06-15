0007501751bd7f0cfaa5ed6b175e29d5208bab9f
leohutnyk <leohutnyk@gmail.com>
2025-06-15 08:46:28 +0200

    next commit

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,36 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { age, username, toggle, empty, callback } from "./basic/1.ts";
+console.log("age:", age);
+console.log("Username:", username);
+console.log(toggle);
+console.log(empty);
+console.log(callback(10));
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import { person } from "./basic/2.ts";
+console.log(person);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+import { variableType } from "./basic/3.ts";
+console.log(variableType);
+import { status } from "./basic/3.ts";
+console.log(status);
+
+import { showMessage } from "./basic/4.ts";
+console.log(showMessage);
+import { calc } from "./basic/4.ts";
+console.log(calc);
+import { customError } from "./basic/4.ts";
+console.log(customError);
+
+import { isWeekend } from "./basic/5.ts";
+console.log(isWeekend);
+import { mango, poly } from "./basic/6.ts";
+console.log(mango, poly);
+import { page1 } from "./basic/7.ts";
+console.log(page1);
+
+import { fetchData } from "./generics/1.ts";
+console.log(fetchData);
+import { compare } from "./generics/2.ts";
+console.log(compare);
+
+import { merge } from "./generics/3.ts";
+console.log(merge);
